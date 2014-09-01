7560a231a80e80daf0c9251023da5648aafc8a16
Johan Haleby <johan.haleby@gmail.com>
2014-09-01 19:00:24 +0200

    Fixed test

--- test/trumpet_server/web_test.clj
+++ test/trumpet_server/web_test.clj
@@ -17,12 +17,12 @@
                      (after :facts (do (.stop @server)
                                        (reset! server nil)))]
                     (fact "Entry point returns the correct resources"
-                          (def href-for-rel (->> (client/get "http://127.0.0.1:5000" {:query-params {"latitude" 22.2 "longtiude" 21.2} :as :json})
+                          (def href-for-rel (->> (client/get "http://127.0.0.1:5000" {:query-params {"latitude" 22.2 "longitude" 21.2} :as :json})
                                                  :body
                                                  find-href-in-response))
-                          (href-for-rel "subscribe") => (just #"^http://127.0.0.1:5000/clients/\w.+/subscribe")
-                          (href-for-rel "location") => (just #"^http://127.0.0.1:5000/clients/\w.+/location")
-                          (href-for-rel "trumpet") => (just #"^http://127.0.0.1:5000/clients/\w.+/trumpet")
+                          (href-for-rel "subscribe") => (just #"^http://127.0.0.1:5000/trumpeters/\d/subscribe")
+                          (href-for-rel "location") => (just #"^http://127.0.0.1:5000/trumpeters/\d/location")
+                          (href-for-rel "trumpet") => (just #"^http://127.0.0.1:5000/trumpeters/\d/trumpet")
                           (href-for-rel "self") => "http://127.0.0.1:5000"))
 
 
