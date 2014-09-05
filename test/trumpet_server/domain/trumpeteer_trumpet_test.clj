127e8ca3e01ec75c6d80b42c42389991e09ec8c6
Johan Haleby <johan.haleby@gmail.com>
2014-09-05 07:00:08 +0200

    Added test for filter-in-range function

--- test/trumpet_server/domain/trumpeteer_trumpet_test.clj
+++ test/trumpet_server/domain/trumpeteer_trumpet_test.clj
@@ -1,6 +1,6 @@
 (ns trumpet-server.domain.trumpeteer-trumpet-test
   (:require [midje.sweet :refer :all]
-            [trumpet-server.domain.trumpeteer :refer :all]))
+            [trumpet-server.domain.trumpeteer :refer [->Trumpeteer]]))
 
 (fact "Send trumpet to all trumpeteers execept self and those that are out-of-range"
       ; Given
@@ -14,9 +14,9 @@
       (def messages-sent (atom {}))
 
       ; When
-      (trumpet! trumpeteer1 {:trumpetees   [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
-                             :broadcast-fn (fn [id message] (swap! messages-sent assoc id message))
-                             :trumpet      "Hello World!"})
+      (.trumpet! trumpeteer1 {:trumpetees   [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
+                              :broadcast-fn (fn [id message] (swap! messages-sent assoc id message))
+                              :trumpet      "Hello World!"})
 
       ; Then
       (keys (deref messages-sent)) => (just (list 2 3 4) :in-any-order))
@@ -33,9 +33,9 @@
       (def messages-sent (atom {}))
 
       ; When
-      (trumpet! trumpeteer1 {:trumpetees   [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
-                             :broadcast-fn (fn [id message] (swap! messages-sent assoc id message))
-                             :trumpet      "Hello World!"})
+      (.trumpet! trumpeteer1 {:trumpetees   [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
+                              :broadcast-fn (fn [id message] (swap! messages-sent assoc id message))
+                              :trumpet      "Hello World!"})
 
       ; Then
       (def messages (vals (deref messages-sent)))
@@ -54,10 +54,10 @@
       (def messages-sent (atom {}))
 
       ; When
-      (trumpet! trumpeteer1 {:trumpetees          [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
-                             :broadcast-fn        (fn [id message] (swap! messages-sent assoc id message))
-                             :trumpet             "Hello World!"
-                             :max-distance-meters 150})
+      (.trumpet! trumpeteer1 {:trumpetees          [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
+                              :broadcast-fn        (fn [id message] (swap! messages-sent assoc id message))
+                              :trumpet             "Hello World!"
+                              :max-distance-meters 150})
       ; Then
       (keys (deref messages-sent)) => (just (list 2 3) :in-any-order))
 
@@ -73,10 +73,10 @@
       (def messages-sent (atom {}))
 
       ; When
-      (trumpet! trumpeteer1 {:trumpetees          [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
-                             :broadcast-fn        (fn [id message] (swap! messages-sent assoc id message))
-                             :trumpet             "Hello World!"
-                             :max-distance-meters nil})
+      (.trumpet! trumpeteer1 {:trumpetees          [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
+                              :broadcast-fn        (fn [id message] (swap! messages-sent assoc id message))
+                              :trumpet             "Hello World!"
+                              :max-distance-meters nil})
       ; Then
       (keys (deref messages-sent)) => (just (list 2 3 4) :in-any-order))
 
@@ -93,10 +93,24 @@
       (def messages-sent (atom {}))
 
       ; When
-      (trumpet! trumpeteer1 {:trumpetees          [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
-                             :broadcast-fn        (fn [id message] (swap! messages-sent assoc id message))
-                             :trumpet             "Hello World!"
-                             :max-distance-meters 100000})
+      (.trumpet! trumpeteer1 {:trumpetees          [trumpeteer1 trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6]
+                              :broadcast-fn        (fn [id message] (swap! messages-sent assoc id message))
+                              :trumpet             "Hello World!"
+                              :max-distance-meters 100000})
       ; Then
       (keys (deref messages-sent)) => (just (list 2 3 4) :in-any-order))
+
+(fact "filter-in-range returns all trumpeteers that are in range"
+      ; Given
+      (def trumpeteer1 (->Trumpeteer 1, 55.583985, 12.957578)) ; self
+      (def trumpeteer2 (->Trumpeteer 2, 55.584126, 12.957406))
+      (def trumpeteer3 (->Trumpeteer 3, 55.58453, 12.957406))
+      (def trumpeteer4 (->Trumpeteer 4, 55.58533, 12.95864))
+      (def trumpeteer5 (->Trumpeteer 5, 55.59772, 12.97519)) ; out of range
+      (def trumpeteer6 (->Trumpeteer 6, 55.60519, 13.00334)) ; out of range
+
+      (let [in-range (.filter-in-range trumpeteer1 [trumpeteer2 trumpeteer3 trumpeteer4 trumpeteer5 trumpeteer6])
+            ids (map #(:id %) in-range)]
+        ids) => (just (list 2 3 4) :in-any-order))
+
 
